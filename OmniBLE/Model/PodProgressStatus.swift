8c9ca2898ad52c0d4c0576f8f68257fd3ebc0ec4
Joe Moran <mojo@moranfoundation.org>
2021-09-26 21:38:53 -0700

    Dash updates for fault codes, Ref codes, and pod progress

--- OmniBLE/Model/PodProgressStatus.swift
+++ OmniBLE/Model/PodProgressStatus.swift
@@ -1,9 +1,10 @@
 //
 //  PodProgressStatus.swift
-//  OmniKit
+//  OmniBLEKit
 //
 //  Created by Pete Schwamb on 9/28/18.
 //  Copyright © 2018 Pete Schwamb. All rights reserved.
+//  Copyright © 2021 Joseph Moran. All rights reserved.
 //
 
 import Foundation
@@ -19,12 +20,12 @@ public enum PodProgressStatus: UInt8, CustomStringConvertible, Equatable {
     case insertingCannula = 7
     case aboveFiftyUnits = 8
     case fiftyOrLessUnits = 9
-    case oneNotUsed = 10
-    case twoNotUsed = 11
-    case threeNotUsed = 12
-    case faultEventOccurred = 13        // fault event occurred (a "screamer")
-    case activationTimeExceeded = 14    // took > 2 hrs from progress 2 to 3 OR > 1 hr from 3 to 8
-    case inactive = 15                  // pod deactivated or a fatal packet state error
+    case faultEventOccurred = 10        // fault event occurred (a "screamer")
+    case activationTimeExceeded = 11    // took > 2 hrs from progress 2 to 3 OR > 1 hr from 3 to 8
+    case inactive = 12                  // pod deactivated or a fatal packet state error
+    case manufacturingTest = 13
+    case unused14 = 14
+    case unused15 = 15
     
     public var readyForDelivery: Bool {
         return self == .fiftyOrLessUnits || self == .aboveFiftyUnits
@@ -52,18 +53,18 @@ public enum PodProgressStatus: UInt8, CustomStringConvertible, Equatable {
             return LocalizedString("Normal", comment: "Pod state when running above fifty units")
         case .fiftyOrLessUnits:
             return LocalizedString("Low reservoir", comment: "Pod state when running with fifty or less units")
-        case .oneNotUsed:
-            return LocalizedString("oneNotUsed", comment: "Pod state oneNotUsed")
-        case .twoNotUsed:
-            return LocalizedString("twoNotUsed", comment: "Pod state twoNotUsed")
-        case .threeNotUsed:
-            return LocalizedString("threeNotUsed", comment: "Pod state threeNotUsed")
         case .faultEventOccurred:
             return LocalizedString("Fault event occurred", comment: "Pod state when fault event has occurred")
         case .activationTimeExceeded:
             return LocalizedString("Activation time exceeded", comment: "Pod state when activation not completed in the time allowed")
         case .inactive:
             return LocalizedString("Deactivated", comment: "Pod state when deactivated")
+        case .manufacturingTest:
+            return LocalizedString("oneNotUsed", comment: "Pod state oneNotUsed")
+        case .unused14:
+            return LocalizedString("twoNotUsed", comment: "Pod state unused 14")
+        case .unused15:
+            return LocalizedString("threeNotUsed", comment: "Pod state unused 15")
         }
     }
 }
